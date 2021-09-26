d41711ae849f0822d3d7e62968cface21993a43c
Zhiwei Tan <zhiweix1988@gmail.com>
2021-09-26 22:04:43 +0800

    solve problem 208

---
+++ 208-implement-trie-prefix-tree/solution.cpp
@@ -0,0 +1,50 @@
+class Trie {
+public:
+    Trie() {
+
+    }
+
+    void insert(string word) {
+        auto *node = &rootNode;
+        for (auto c : word) {
+            if (!node->nodes.count(c)) {
+                node->nodes[c] = new TrieNode();
+            }
+            node = node->nodes[c];
+        }
+
+        node->isWord = true;
+    }
+
+    bool search(string word) {
+        auto *node = &rootNode;
+        for (auto c : word) {
+            if (!node->nodes.count(c)) {
+                return false;
+            }
+            node = node->nodes[c];
+        }
+
+        return node->isWord;
+    }
+
+    bool startsWith(string prefix) {
+        auto *node = &rootNode;
+        for (auto c : prefix) {
+            if (!node->nodes.count(c)) {
+                return false;
+            }
+            node = node->nodes[c];
+        }
+
+        return true;
+    }
+
+private:
+    struct TrieNode {
+        unordered_map<char, TrieNode*> nodes;
+        bool isWord = false;
+    };
+
+    TrieNode rootNode;
+};
