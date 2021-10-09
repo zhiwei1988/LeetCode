89f3c11e39622706b54fd2881efc7580be1fa83c
Zhiwei Tan <zhiweix1988@gmail.com>
2021-10-09 23:31:52 +0800

    solve problem 503

---
+++ 503-next-greater-element-ii/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        stack<int> s;
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!s.empty() && s.top() <= nums[i%n]) {
+                s.pop();
+            }
+
+            if (s.empty()) {
+                res[i%n] = -1;
+            } else {
+                res[i%n] = s.top();
+            }
+
+            s.push(nums[i%n]);   
+        }
+        return res;
+    }
+};
